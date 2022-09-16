ae061e7630b7ab6254845667b762dbadc3951649
montassar <montassar.bouhjila@softparadigm.com>
2022-09-16 22:30:53 +0100

    java 8 features

---
+++ src/main/java/javafeatures/FunctionalInterfaceDemo.java
@@ -0,0 +1,43 @@
+package javafeatures;
+
+@FunctionalInterface
+interface MyFunctionalInterface {
+
+    static boolean isEmpty(String string) {
+        if (string == null) {
+            return true;
+        }
+        return true;
+    }
+
+    String concatString(String[] strings);
+    /*  test method can't exist with the presence of @FunctionalInterface annotaion*/
+    //void test();
+
+    default boolean hasText(String string) {
+        if (string == null) return false;
+
+        if (string.trim().length() == 0) {
+            return true;
+        }
+        return true;
+
+    }
+}
+
+public class FunctionalInterfaceDemo {
+
+    public static void main(String[] args) {
+        String[] strings = new String[]{"Hello", " ", "word!"};
+        /*  Use of lambda expression to instantiate myFunctionalInterface */
+        MyFunctionalInterface myFunctionalInterface = (String[] arrayString) -> {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String element : arrayString) {
+                stringBuilder.append(element);
+            }
+            return stringBuilder.toString();
+        };
+        System.out.println(myFunctionalInterface.concatString(strings));
+    }
+}
+
---
+++ src/main/java/javafeatures/StringJoinerDemo.java
@@ -0,0 +1,24 @@
+package javafeatures;
+
+import java.util.StringJoiner;
+
+public class StringJoinerDemo {
+    public static void main(String[] args) {
+        StringJoiner stringJoinerWhiteSpaceDelimiter = new StringJoiner(" ");
+        stringJoinerWhiteSpaceDelimiter.add("Hello");
+        stringJoinerWhiteSpaceDelimiter.add("World");
+        stringJoinerWhiteSpaceDelimiter.add("!");
+
+        StringJoiner stringJoiner = new StringJoiner(" ", "[","]");
+        stringJoiner.add("TN");
+        stringJoiner.add("FR");
+        stringJoiner.add("USA");
+        stringJoiner.add("UK");
+
+        System.out.println(stringJoinerWhiteSpaceDelimiter);
+        System.out.println(stringJoiner);
+        System.out.println(stringJoiner.merge(stringJoinerWhiteSpaceDelimiter));
+        System.out.println(stringJoiner.merge(stringJoinerWhiteSpaceDelimiter));
+
+    }
+}
