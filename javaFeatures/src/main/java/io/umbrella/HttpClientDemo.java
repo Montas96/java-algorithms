012e7365a01a13e887bb017f94f66e36e9997269
montassar <montassar.bouhjila@softparadigm.com>
2022-10-15 21:21:42 +0100

    http client

--- javaFeatures/src/main/java/io/umbrella/HttpClientDemo.java
+++ javaFeatures/src/main/java/io/umbrella/HttpClientDemo.java
@@ -5,13 +5,24 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class HttpClientDemo {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
+
+        final String URL = "http://heeroplatform.com/api/product-families/DEFAULT_FAMILY/FRENCH_FRANCE";
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create("http://heeroplatform.com/api/product-families/DEFAULT_FAMILY/FRENCH_FRANCE")).build();
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(URL)).build();
         HttpResponse response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
+        System.out.println("****");
+        CompletableFuture<Void> asyncResponse = client.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println);
+        asyncResponse.get();
+        System.out.println("****");
+
     }
 }
